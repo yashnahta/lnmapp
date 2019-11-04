d5e7df92e1c01243cffc28082d6fb8f22b228de8
yashnahta <yashnahtanaman12345@gmail.com>
2019-11-04 22:10:46 +0530

    yo

--- app/src/main/java/com/example/lnmapp/Y17CSE.java
+++ app/src/main/java/com/example/lnmapp/Y17CSE.java
@@ -3,11 +3,19 @@ package com.example.lnmapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -15,6 +23,9 @@ import android.view.ViewGroup;
  */
 public class Y17CSE extends Fragment {
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference mDatabase;
+// ...
 
     public Y17CSE() {
         // Required empty public constructor
@@ -25,6 +36,22 @@ public class Y17CSE extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+        mDatabase = database.getReference().child("CLASS TIMETABLE").child("MONDAY").child("10AM-11AM");
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(getActivity(),"yoo    "+dataSnapshot.getValue(String.class),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         return inflater.inflate(R.layout.fragment_y17_cse, container, false);
     }
 
