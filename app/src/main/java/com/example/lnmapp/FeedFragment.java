858b98f45b41ba0f06b6df3c186e91e4489b2856
yashnahta <yashnahtanaman12345@gmail.com>
2019-11-11 20:19:21 +0530

    feed daalo

--- app/src/main/java/com/example/lnmapp/FeedFragment.java
+++ app/src/main/java/com/example/lnmapp/FeedFragment.java
@@ -4,6 +4,7 @@ package com.example.lnmapp;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,14 +13,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
 
 public class FeedFragment extends Fragment {
 
-
+View view;
     private TextView textView2;
     private TextView textView3;
     private TextView textView4;
-
+    String[] arrSplit;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference mDatabase;
     public FeedFragment() {
         // Required empty public constructor
     }
@@ -30,34 +43,80 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        view = inflater.inflate(R.layout.fragment_feed, container, false);
 
         String str = "dwekn.kcmlamsmwldn";
 
-        TextView textView ;
-        textView = view.findViewById(R.id.t1);
-        textView.setText("parth");
+        mDatabase = database.getReference().child("FEED").child("feeds");
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-        textView2 = view.findViewById(R.id.t2);
-        textView2.setText("lahbfecnw");
+                String s = dataSnapshot.getValue(String.class);
+                arrSplit = s.split("--");
+
+                            TextView textView ;
+        textView = view.findViewById(R.id.t1);
+        textView.setText(arrSplit[0]);
+                textView2 = view.findViewById(R.id.t2);
+        textView2.setText(arrSplit[1]);
 
         textView3 = view.findViewById(R.id.t3);
-        textView3.setText("wjdbec");
+        textView3.setText(arrSplit[2]);
 
         textView4 = view.findViewById(R.id.t4);
-        textView4.setText(str);
+        textView4.setText(arrSplit[3]);
 
         textView = view.findViewById(R.id.t5);
-        textView.setText("parth");
+        textView.setText(arrSplit[4]);
 
         textView = view.findViewById(R.id.t6);
-        textView.setText("lahbfecnw");
+        textView.setText(arrSplit[5]);
 
         textView = view.findViewById(R.id.t7);
-        textView.setText("wjdbec");
+        textView.setText(arrSplit[6]);
 
         textView = view.findViewById(R.id.t8);
-        textView.setText("swdefcndj");
+        textView.setText(arrSplit[7]);
+                   // Toast.makeText(getActivity(),arrSplit.length+"  1  "+arrSplit[i],Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+//        for (int i=0; i < arrSplit.length; i++)
+//        {
+//            Toast.makeText(getActivity(),arrSplit.length+"  1  "+arrSplit[i],Toast.LENGTH_SHORT).show();
+//        }
+
+//        TextView textView ;
+//        textView = view.findViewById(R.id.t1);
+//        textView.setText(arrSplit[0]);
+//
+//        textView2 = view.findViewById(R.id.t2);
+//        textView2.setText(arrSplit[1]);
+//
+//        textView3 = view.findViewById(R.id.t3);
+//        textView3.setText(arrSplit[2]);
+//
+//        textView4 = view.findViewById(R.id.t4);
+//        textView4.setText(arrSplit[3]);
+//
+//        textView = view.findViewById(R.id.t5);
+//        textView.setText(arrSplit[4]);
+//
+//        textView = view.findViewById(R.id.t6);
+//        textView.setText(arrSplit[5]);
+//
+//        textView = view.findViewById(R.id.t7);
+//        textView.setText(arrSplit[6]);
+//
+//        textView = view.findViewById(R.id.t8);
+//        textView.setText(arrSplit[7]);
 
 
         return view;
