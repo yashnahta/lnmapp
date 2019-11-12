1135320a1517f51b440253828c19a0dbf7dfb150
Aditya Rathi <adityarathi42@gmail.com>
2019-11-13 03:26:32 +0530

    yelo bc

--- app/src/main/java/com/example/lnmapp/Complaint.java
+++ app/src/main/java/com/example/lnmapp/Complaint.java
@@ -1,27 +1,45 @@
 package com.example.lnmapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Complaint extends Fragment {
     public Complaint() {
         // Required empty public constructor
     }
 
-
+Button dial;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_complaint, container, false);
         // Inflate the layout for this fragment
         getActivity().setTitle("Make Complaints");
-        return inflater.inflate(R.layout.fragment_complaint, container, false);
+
+        dial = view.findViewById(R.id.dial);
+
+
+        return view;
+    }
+    public void onDialButton(View view) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:9680850226"));
+    if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+        return;
+    }
+        startActivity(intent);
     }
 
 }
