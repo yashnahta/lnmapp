c42f4b679ffa493b910ca29f7db92c6d502557e5
yashnahta <yashnahtanaman12345@gmail.com>
2019-11-13 18:05:14 +0530

    notification done

--- app/src/main/java/com/example/lnmapp/HomeActivity.java
+++ app/src/main/java/com/example/lnmapp/HomeActivity.java
@@ -134,7 +134,7 @@ public class HomeActivity extends AppCompatActivity  {
                                 dl.closeDrawer(GravityCompat.START);
                                 break;
                             case R.id.settings:
-                                Toast.makeText(HomeActivity.this, " Settings",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(HomeActivity.this, "Log Out",Toast.LENGTH_SHORT).show();
                                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Settings()).commit();
                                 dl.closeDrawer(GravityCompat.START);
                                 break;
